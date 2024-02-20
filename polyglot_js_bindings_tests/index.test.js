57a146e2104a2521eceb6cc3369b28f9fd2e018e
Adam Grabski <adam.gr@outlook.com>
2024-02-20 22:08:23 +0000

    fixed package publishing

--- polyglot_js_bindings_tests/index.test.js
+++ polyglot_js_bindings_tests/index.test.js
@@ -1,16 +1,16 @@
-import evaluate_expression from './index';
+import { evaluate } from "polyglot_js/index.js";
 
 test('Basic expression with parameters evaluates correctly', () => {
     const expression = '(= @a 1)';
     const parameters = { a: '1' };
-    const result = evaluate_expression(expression, parameters);
+    const result = evaluate(expression, parameters);
     expect(result).toBe(true);
 });
 
 test('Basic expression with constants evaluates correctly', () => {
     const expression = '(= 1 1)';
     const parameters = {};
-    const result = evaluate_expression(expression, parameters);
+    const result = evaluate(expression, parameters);
     expect(result).toBe(true);
 });
 
